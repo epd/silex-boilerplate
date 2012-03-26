d02d2dacce75de964c29454e0014b76cfbcd2de5
Evan Dudla <me@evandudla.com>
2012-03-26 02:29:31 -0400

    Added cd to get script

--- get.sh
+++ get.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 git clone --recursive git@github.com:evNN/silex-boilerplate.git
+cd silex-boilerplate
 wget http://silex.sensiolabs.org/get/silex.phar
 curl -s http://getcomposer.org/installer | php ; php composer.phar install
