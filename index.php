d4f824a4cc95ca99571a18080aea9174a32b9386
Evan Dudla <me@evandudla.com>
2012-03-27 03:26:50 -0400

    Added LESS minification for production

--- index.php
+++ index.php
@@ -8,9 +8,8 @@
 require_once __DIR__ . '/silex.phar';
 $app = new Silex\Application();
 
-// Symfony Request and Response components
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+// Turn Silex debugging on
+$app['debug'] = TRUE;
 
 // Register namespace for Silex Extensions
 $app['autoloader']->registerNamespace('SilexExtension', __DIR__ . '/vendor/silex-extension/src');
@@ -19,6 +18,10 @@ $app['autoloader']->registerNamespace('LessElephant', __DIR__ . '/vendor/cypress
 $app['autoloader']->registerNamespace('Symfony\\Component\\Finder', __DIR__ . '/vendor/symfony/finder');
 $app['autoloader']->registerNamespace('Symfony\\Component\\Process', __DIR__ . '/vendor/symfony/process');
 
+// Symfony Request and Response components
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Process\Process;
 
 // Include Monolog to add application debug logging
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
@@ -45,18 +48,27 @@ $app->register(new evNN\Silex\Less\LessServiceProvider(), array(
   'less.less_dir' => __DIR__ . '/assets/less',
   'less.app_less' => 'app.less',
   'less.app_css'  => __DIR__ . '/assets/css/app.css',
+  'less.flags'    => '--yui-compress',
 ));
-
-// Turn Silex debugging on
-$app['debug'] = TRUE;
 
 // Inject our application routes
 require_once __DIR__ . "/app/routes.php";
 
 // Compile LESS files in a "before" filter
 $app->before(function () use($app) {
+
+  // Check if we need to compile or not
   if (!$app['less']->isClean()) {
-    $app['less']->compile();
+    // Execute our compile command with optional debug parameters
+    $cmd = sprintf('%s' . (!$app['debug'] ? ' ' . $app['less.flags'] : '') . ' %s > %s', $app['less']->getLessBinary()->getPath(), $app['less']->getSourceFolder() . '/' . $app['less']->getSourceFile(), $app['less']->getDestination());
+    $process = new Process($cmd, $app['less']->getSourceFolder());
+    $process->run();
+
+    // Throw error in debug mode
+    if ($app['debug'] && !$process->isSuccessful()) {
+      $app['monolog']->addError('LESS compilation failed. Turn on debug mode for more info.');
+      throw new \RuntimeException($process->getErrorOutput());
+    }
   }
 });
 
