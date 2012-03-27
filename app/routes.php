d06b02eb9f3f7bed99591da537925062401fd330
Evan Dudla <me@evandudla.com>
2012-03-27 15:28:11 -0400

    Updated route and default layout/css

--- app/routes.php
+++ app/routes.php
@@ -6,11 +6,14 @@
 
 // Render our default route
 $app->get('/', function() use($app) {
+  // An example of using Monolog for debugging
   if ($app['debug']) {
     $app['monolog']->addInfo('Getting the default layout.');
   }
+
+  // Render our main layout here using the Twig component
   return $app['twig']->render('layout.twig', array(
-    'title' => 'My Web App',
+    'title' => 'My Super Cool App',
     'app_css' => '/app/assets/css/app.css',
   ));
 });
