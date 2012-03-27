0ad314e5c6a09a4dfb30a3f0892e3186b07840e5
Evan <evan.dudla@gmail.com>
2012-03-27 16:30:47 -0300

    Updated get.sh script to operate within system/ directory

--- system/get.sh
+++ system/get.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 git clone --recursive git@github.com:evNN/silex-boilerplate.git
-cd silex-boilerplate
+cd silex-boilerplate/system
 curl -C - -O http://silex.sensiolabs.org/get/silex.phar
 curl -s http://getcomposer.org/installer | php ; php composer.phar install
